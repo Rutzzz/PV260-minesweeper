66a9bdedba8bd173151d0dab8865efd42c50594c
Ondrej Machala <ondra097@seznam.cz>
2021-03-24 20:54:11 +0100

    Add check for correct dimensions

--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -8,7 +8,8 @@ namespace Minesweeper
     {
         public Game(bool[,] mines)
         {
-            
+            if (mines.GetLength(0) < 3 || mines.GetLength(0) > 50 || mines.GetLength(1) < 3 || mines.GetLength(1) > 50)
+                throw new ArgumentException("Invalid mines array dimension");
         }
     }
 }
