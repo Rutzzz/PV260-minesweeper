e913c73ecf770de1ae76d6fa973ee08579e556f7
Štěpán Horáček <shoracek@redhat.com>
2021-03-24 22:16:10 +0100

    Refactor CreateFilledBoard

--- Minesweeper.Tests/GameTest.cs
+++ Minesweeper.Tests/GameTest.cs
@@ -32,7 +32,7 @@ namespace Minesweeper.Tests
         {
             bool[,] mines = new bool[nrow, ncol];
             Game game = new Game(mines);
-            Assert.That(game.Board, Is.EqualTo(CreateEmptyBoard(nrow, ncol)));
+            Assert.That(game.Board, Is.EqualTo(CreateFilledBoard(nrow, ncol, '.')));
         }
 
         [TestCase(1, 1, 1, 2)]
@@ -40,7 +40,7 @@ namespace Minesweeper.Tests
         [TestCase(3, 3, 2, 2)]
         public void Uncover_GivenStepsNextToMine_FieldWithNumber(int mineRow, int mineCol, int posRow, int posCol)
         {
-            char[,] expectedBoard = CreateEmptyBoard(4, 4);
+            char[,] expectedBoard = CreateFilledBoard(4, 4, '.');
             expectedBoard[posRow - 1, posCol - 1] = '1';
             bool[,] mines = new bool[4, 4];
             mines[mineRow - 1, mineCol - 1] = true;
@@ -76,14 +76,14 @@ namespace Minesweeper.Tests
         }
 
 
-        private char[,] CreateEmptyBoard(int nrow, int ncol)
+        private char[,] CreateFilledBoard(int nrow, int ncol, char cellValue)
         {
             char[,] board = new char[nrow, ncol];
             for (int r = 0; r < nrow; r++)
             {
                 for (int c = 0; c < ncol; c++)
                 {
-                    board[r, c] = '.';
+                    board[r, c] = cellValue;
                 }
             }
 
