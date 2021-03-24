f18fda51c8ab455777afe9283f109b922a596a9d
Ondrej Machala <ondra097@seznam.cz>
2021-03-24 20:47:01 +0100

    Add game class

---
+++ Minesweeper/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minesweeper
+{
+    public class Game
+    {
+        public Game(bool[,] mines)
+        {
+            
+        }
+    }
+}
