a134a903fca8f4ed1b6b61534718b17de679a899
Ondrej Machala <ondra097@seznam.cz>
2021-03-24 20:50:24 +0100

    Add test for incorrect dimensions

--- Minesweeper.Tests/GameTest.cs
+++ Minesweeper.Tests/GameTest.cs
@@ -12,5 +12,18 @@ namespace Minesweeper.Tests
             bool[,] mines = new bool[3, 3];
             new Game(mines);
         }
+
+        [TestCase(2,2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 2)]
+        [TestCase(51, 51)]
+        [TestCase(51, 50)]
+        [TestCase(50, 51)]
+        public void Constructor_GivenIncorrectDimensions_ThrowException(int nrow, int ncol)
+        {
+            bool[,] mines = new bool[nrow, ncol];
+            Assert.Throws<ArgumentException>(() => new Game(mines));
+        }
+
     }
 }
