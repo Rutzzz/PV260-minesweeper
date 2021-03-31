1a5b461c134222787781a66a3c94bf368a910236
Cestmir Kalina <46683072+ckalina@users.noreply.github.com>
2021-03-31 23:26:48 +0200

    Add generate invalid density throw exception test
    Signed-off-by: Cestmir Kalina <46683072+ckalina@users.noreply.github.com>

--- Minesweeper.Tests/BoardGeneratorTest.cs
+++ Minesweeper.Tests/BoardGeneratorTest.cs
@@ -13,5 +13,15 @@ namespace Minesweeper.Tests
                     BoardGenerator.Generate(numberOfRows, numberOfColumns, 0.5f)
             );
         }
+        
+        [Test]
+        [TestCase(0.7f)]
+        [TestCase(0.1f)]
+        public void Generate_InvalidDensity_Throw(float density)
+        {
+            Assert.Throws<ArgumentException>(() => 
+                BoardGenerator.Generate(11, 10, density)
+            );
+        }
     }
 }
