7def7e292a29be8da0fe1dba7b3a9cadfeafa327
Cestmir Kalina <46683072+ckalina@users.noreply.github.com>
2021-03-31 23:28:47 +0200

    Implement density test in generate
    Signed-off-by: Cestmir Kalina <46683072+ckalina@users.noreply.github.com>

--- Minesweeper/BoardGenerator.cs
+++ Minesweeper/BoardGenerator.cs
@@ -8,6 +8,10 @@ namespace Minesweeper
         {
             if (numberOfRows < 1 || numberOfColumns < 1)
                 throw new ArgumentException("Invalid dimensions given.");
+
+            if (density < 0.2 || density > 0.6)
+                throw new ArgumentException("Invalid mine density given.");
+            
             return null;
         }
     }
