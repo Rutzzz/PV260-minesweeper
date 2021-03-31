e766c86277e78bf9cdecae028afc1316808a17d9
Cestmir Kalina <46683072+ckalina@users.noreply.github.com>
2021-03-31 23:58:58 +0200

    Add random mine placement constructor invalid dimension test
    Signed-off-by: Cestmir Kalina <46683072+ckalina@users.noreply.github.com>

--- Minesweeper.Tests/BoardGeneratorTest.cs
+++ Minesweeper.Tests/BoardGeneratorTest.cs
@@ -3,6 +3,16 @@ using NUnit.Framework;
 
 namespace Minesweeper.Tests
 {
+    public class RandomMinePlacementGeneratorTests
+    {
+        [Test]
+        [TestCase(-50,-50)]
+        public void Constructor_InvalidDimensions_Throws(int numberOfRows, int numberOfCols)
+        {
+            Assert.Throws<ArgumentException>(() => new RandomMinePlacementGenerator(numberOfRows, numberOfCols));
+        }
+    }
+
     public class BoardGeneratorTest
     {
         [Test]
