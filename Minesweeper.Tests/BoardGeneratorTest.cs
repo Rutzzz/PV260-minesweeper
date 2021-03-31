72f912612da33869b86cab5f3219f342d1595325
Cestmir Kalina <46683072+ckalina@users.noreply.github.com>
2021-03-31 23:12:04 +0200

    Add generate invalid dimension throws exception test
    Signed-off-by: Cestmir Kalina <46683072+ckalina@users.noreply.github.com>

--- Minesweeper.Tests/BoardGeneratorTest.cs
+++ Minesweeper.Tests/BoardGeneratorTest.cs
@@ -5,6 +5,13 @@ namespace Minesweeper.Tests
 {
     public class BoardGeneratorTest
     {
-        
+        [Test]
+        [TestCase(-1,-1)]
+        public void Generate_InvalidDimension_ThrowsException(int numberOfRows, int numberOfColumns)
+        {
+            Assert.Throws<ArgumentException>( ( ) =>
+                    BoardGenerator.Generate(numberOfRows, numberOfColumns, 0.5f)
+            );
+        }
     }
 }
